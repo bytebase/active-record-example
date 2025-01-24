c4b04591bd1a6a49e8eb605142c9b9d09c1181a7
tianzhou <t@bytebase.com>
2025-01-24 16:52:32 +0800

    chore: cleanup migration files

--- db/migrate/20240320121000_create_salary.sql
+++
@@ -1,9 +0,0 @@
---
--- CreateSalary : 20240320121000
---
-
-BEGIN;
-
-CREATE TABLE "salary" ("id" bigserial primary key, "name" character varying, "amount" integer, "created_at" timestamp(6) NOT NULL, "updated_at" timestamp(6) NOT NULL);
-
-INSERT INTO schema_migrations (version) VALUES ('20240320121000');
