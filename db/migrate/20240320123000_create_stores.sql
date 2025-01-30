a62dd596f5e8a7ef07052532a05436f87fc34617
Tianzhou <t@bytebase.com>
2025-01-31 01:33:51 +0800

    tweak

--- db/migrate/20240320123000_create_stores.sql
+++ db/migrate/20240320123000_create_stores.sql
@@ -4,7 +4,7 @@
 
 BEGIN;
 
-CREATE TABLE "stores" ("id" bigserial primary key, "name" character varying, "created_at" timestamp(6) NOT NULL, "updated_at" timestamp(6) NOT NULL);
+CREATE TABLE "stores" ("id" bigserial primary key, "name" character varying NOT NULL, "created_at" timestamp(6) NOT NULL, "updated_at" timestamp(6) NOT NULL);
 
 CREATE UNIQUE INDEX "index_stores_on_name" ON "stores" ("name");
 
